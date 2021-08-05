0d6abb4326101191e1dbd88dc5e323f35243f187
Jan Oppolzer <jan@oppolzer.cz>
2021-08-05 11:04:30 +0200

    Tweak navigation.blade.php

--- resources/views/entities/navigation.blade.php
+++ resources/views/entities/navigation.blade.php
@@ -1,5 +1,20 @@
 <div class="mb-4">
-    <a class="px-2 py-1 mr-2 bg-blue-100 hover:bg-blue-300 rounded-lg shadow" href="{{ route('entities.show', $entity)}}">{{ __('common.details') }}</a>
-    <a class="px-2 py-1 mr-2 bg-blue-100 hover:bg-blue-300 rounded-lg shadow" href="{{ route('entities.operators', $entity)}}">{{ __('common.operators') }}</a>
-    <a class="px-2 py-1 mr-2 bg-blue-100 hover:bg-blue-300 rounded-lg shadow" href="{{ route('entities.federations', $entity)}}">{{ __('common.federations') }}</a>
+    <a @class([
+        'px-2', 'py-1', 'mr-2', 'rounded-lg', 'shadow',
+        'bg-gray-300' => request()->routeIs('entities.show'),
+        'bg-blue-100 hover:bg-blue-300' => !request()->routeIs('entities.show'),
+        'cursor-default' => request()->routeIs('entities.show'),
+    ]) href="{{ route('entities.show', $entity)}}">{{ __('common.details') }}</a>
+    <a @class([
+        'px-2', 'py-1', 'mr-2', 'rounded-lg', 'shadow',
+        'bg-gray-300' => request()->routeIs('entities.operators'),
+        'bg-blue-100 hover:bg-blue-300' => !request()->routeIs('entities.operators'),
+        'cursor-default' => request()->routeIs('entities.operators'),
+    ]) href="{{ route('entities.operators', $entity)}}">{{ __('common.operators') }}</a>
+    <a @class([
+        'px-2', 'py-1', 'mr-2', 'rounded-lg', 'shadow',
+        'bg-gray-300' => request()->routeIs('entities.federations'),
+        'bg-blue-100 hover:bg-blue-300' => !request()->routeIs('entities.federations'),
+        'cursor-default' => request()->routeIs('entities.federations'),
+    ]) href="{{ route('entities.federations', $entity)}}">{{ __('common.federations') }}</a>
 </div>
