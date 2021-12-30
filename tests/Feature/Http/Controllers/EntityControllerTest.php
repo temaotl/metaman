27c19b179280f925c1685a47bbb94be90bd25e75
Jan Oppolzer <jan@oppolzer.cz>
2021-12-30 17:53:20 +0100

    Fix EntityControllerTest
    Fixed Jetty's cipher suites.

--- tests/Feature/Http/Controllers/EntityControllerTest.php
+++ tests/Feature/Http/Controllers/EntityControllerTest.php
@@ -309,7 +309,7 @@ class EntityControllerTest extends TestCase
             ->followingRedirects()
             ->actingAs($user)
             ->post(route('entities.store', [
-                'url' => 'https://ratamahatta.cz/whoami.cesnet.cz.xml',
+                'url' => 'https://whoami.cesnet.cz/idp/shibboleth',
                 'federation' => $federation->id,
                 'explanation' => $this->faker->catchPhrase(),
             ]))
@@ -363,7 +363,7 @@ class EntityControllerTest extends TestCase
             ->actingAs($user)
             ->patch(route('entities.update', $entity), [
                 'action' => 'update',
-                'url' => 'https://ratamahatta.cz/whoami.cesnet.cz.xml',
+                'url' => 'https://whoami.cesnet.cz/idp/shibboleth',
             ])
             ->assertSeeText(__('entities.entity_updated'));
 
@@ -687,7 +687,7 @@ class EntityControllerTest extends TestCase
             ->followingRedirects()
             ->actingAs($admin)
             ->post(route('entities.store'), [
-                'url' => 'https://ratamahatta.cz/whoami.cesnet.cz.xml',
+                'url' => 'https://whoami.cesnet.cz/idp/shibboleth',
                 'federation' => $federation->id,
                 'explanation' => $this->faker->catchPhrase(),
             ])
@@ -740,7 +740,7 @@ class EntityControllerTest extends TestCase
             ->actingAs($admin)
             ->patch(route('entities.update', $entity), [
                 'action' => 'update',
-                'url' => 'https://ratamahatta.cz/whoami.cesnet.cz.xml',
+                'url' => 'https://whoami.cesnet.cz/idp/shibboleth',
             ])
             ->assertSeeText(__('entities.entity_updated'));
 
