48d64d8e5754550c132a9bda4cde1c71badccf09
Jan Oppolzer <jan@oppolzer.cz>
2022-03-30 20:08:32 +0200

    Fix grammar

--- resources/views/emails/ask_rs.blade.php
+++ resources/views/emails/ask_rs.blade.php
@@ -1,4 +1,4 @@
-An entity {{ $entity->entityid }} is asking for R&S entity category.
+The entity {{ $entity->entityid }} is asking for R&S entity category.
 
 Go to {{ route('entities.show', $entity) }} and assign R&S manually.
 
