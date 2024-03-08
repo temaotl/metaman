a13c2cefa19991c0af230ebbcbfecb6eca6417c0
Artem Otliaguzov <Artem.Otliaguzov@cesnet.cz>
2024-03-08 11:08:34 +0100

    I have done the templating of all import.blade.php

--- resources/views/categories/import.blade.php
+++ resources/views/categories/import.blade.php
@@ -1,59 +1,53 @@
-@extends('layout')
+@extends('layouts.defaultImport')
 @section('title', __('categories.import'))
+@section('form_action',route('categories.import'))
 
-@section('content')
 
-    <form action="{{ route('categories.import') }}" method="post">
-        @csrf
-        <div class="dark:bg-transparent overflow-x-auto bg-white border rounded-lg">
-            <table class="min-w-full border-b border-gray-300">
-                <thead>
-                    <tr>
-                        <th
-                            class="dark:bg-gray-700 px-6 py-3 text-xs tracking-widest text-left uppercase bg-gray-100 border-b">
-                            &nbsp;
-                        </th>
-                        <th
-                            class="dark:bg-gray-700 px-6 py-3 text-xs tracking-widest text-left uppercase bg-gray-100 border-b">
-                            {{ __('common.file') }}
-                        </th>
-                        <th
-                            class="dark:bg-gray-700 px-6 py-3 text-xs tracking-widest text-left uppercase bg-gray-100 border-b">
-                            {{ __('common.name') }}
-                        </th>
-                        <th
-                            class="dark:bg-gray-700 px-6 py-3 text-xs tracking-widest text-left uppercase bg-gray-100 border-b">
-                            {{ __('common.description') }}
-                        </th>
-                    </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-200">
-                    @foreach ($categories as $category)
-                        <tr x-data class="hover:bg-blue-50 dark:hover:bg-gray-700"
-                            @click="checkbox = $el.querySelector('input[type=checkbox]'); checkbox.checked = !checkbox.checked">
-                            <td class="px-6 py-3 text-sm">
-                                <input @click.stop class="rounded" type="checkbox" name="categories[]"
-                                    value="{{ $category }}">
-                            </td>
-                            <td class="px-6 py-3 text-sm">
-                                <code class="text-sm text-pink-500">
-                                    {{ $category }}
-                                </code>
-                            </td>
-                            <td class="px-6 py-3 text-sm">
-                                <input class="rounded" type="text" name="names[{{ $category }}]">
-                            </td>
-                            <td class="px-6 py-3 text-sm">
-                                <input class="rounded" type="text" name="descriptions[{{ $category }}]">
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="dark:bg-transparent px-4 py-4 bg-gray-100">
-            <x-button>{{ __('categories.import') }}</x-button>
-        </div>
-    </form>
+@section('specific_head_fields')
+    <th
+        class="dark:bg-gray-700 px-6 py-3 text-xs tracking-widest text-left uppercase bg-gray-100 border-b">
+        {{ __('common.description') }}
+    </th>
+
+    <th
+        class="dark:bg-gray-700 px-6 py-3 text-xs tracking-widest text-left uppercase bg-gray-100 border-b">
+        {{ __('common.file') }}
+    </th>
 
 @endsection
+
+@section('specific_fields')
+    @foreach ($categories as $category)
+        <tr x-data class="hover:bg-blue-50 dark:hover:bg-gray-700"
+            @click="checkbox = $el.querySelector('input[type=checkbox]'); checkbox.checked = !checkbox.checked">
+            <td class="px-6 py-3 text-sm">
+                <input @click.stop class="rounded" type="checkbox" name="categories[]"
+                       value="{{ $category }}"  x-bind:checked="selectAll" >
+            </td>
+
+            <td class="px-6 py-3 text-sm">
+                <input class="rounded" type="text" name="names[{{ $category }}]">
+            </td>
+
+            <td class="px-6 py-3 text-sm">
+                <input class="rounded" type="text" name="descriptions[{{ $category }}]">
+            </td>
+
+            <td class="px-6 py-3 text-sm">
+                <code class="text-sm text-pink-500">
+                    {{ $category }}
+                </code>
+            </td>
+
+
+        </tr>
+    @endforeach
+@endsection
+
+@section('submit_button')
+    <x-button>{{ __('categories.import') }}</x-button>
+@endsection
+
+
+
+
--- resources/views/groups/import.blade.php
+++ resources/views/groups/import.blade.php
@@ -1,59 +1,56 @@
-@extends('layout')
+@extends('layouts.defaultImport')
 @section('title', __('groups.import'))
+@section('form_action',route('groups.import'))
 
-@section('content')
 
-    <form action="{{ route('groups.import') }}" method="post">
-        @csrf
-        <div class="dark:bg-transparent overflow-x-auto bg-white border rounded-lg">
-            <table class="min-w-full border-b border-gray-300">
-                <thead>
-                    <tr>
-                        <th
-                            class="dark:bg-gray-700 px-6 py-3 text-xs tracking-widest text-left uppercase bg-gray-100 border-b">
-                            &nbsp;
-                        </th>
-                        <th
-                            class="dark:bg-gray-700 px-6 py-3 text-xs tracking-widest text-left uppercase bg-gray-100 border-b">
-                            {{ __('common.file') }}
-                        </th>
-                        <th
-                            class="dark:bg-gray-700 px-6 py-3 text-xs tracking-widest text-left uppercase bg-gray-100 border-b">
-                            {{ __('common.name') }}
-                        </th>
-                        <th
-                            class="dark:bg-gray-700 px-6 py-3 text-xs tracking-widest text-left uppercase bg-gray-100 border-b">
-                            {{ __('common.description') }}
-                        </th>
-                    </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-200">
-                    @foreach ($groups as $group)
-                        <tr x-data class="hover:bg-blue-50 dark:hover:bg-gray-700"
-                            @click="checkbox = $el.querySelector('input[type=checkbox]'); checkbox.checked = !checkbox.checked">
-                            <td class="px-6 py-3 text-sm">
-                                <input @click.stop class="rounded" type="checkbox" name="groups[]"
-                                    value="{{ $group }}">
-                            </td>
-                            <td class="px-6 py-3 text-sm">
-                                <code class="text-sm text-pink-500">
-                                    {{ $group }}
-                                </code>
-                            </td>
-                            <td class="px-6 py-3 text-sm">
-                                <input class="rounded-lg" type="text" name="names[{{ $group }}]">
-                            </td>
-                            <td class="px-6 py-3 text-sm">
-                                <input class="rounded-lg" type="text" name="descriptions[{{ $group }}]">
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="dark:bg-transparent px-4 py-4 bg-gray-100">
-            <x-button>{{ __('groups.import') }}</x-button>
-        </div>
-    </form>
+@section('specific_head_fields')
+    <th
+        class="dark:bg-gray-700 px-6 py-3 text-xs tracking-widest text-left uppercase bg-gray-100 border-b">
+        {{ __('common.description') }}
+    </th>
+
+    <th
+        class="dark:bg-gray-700 px-6 py-3 text-xs tracking-widest text-left uppercase bg-gray-100 border-b">
+        {{ __('common.file') }}
+    </th>
 
 @endsection
+
+@section('specific_fields')
+
+    @foreach ($groups as $group)
+        <tr x-data class="hover:bg-blue-50 dark:hover:bg-gray-700"
+            @click="checkbox = $el.querySelector('input[type=checkbox]'); checkbox.checked = !checkbox.checked">
+            <td class="px-6 py-3 text-sm">
+                <input @click.stop class="rounded" type="checkbox" name="groups[]"
+                       value="{{ $group }}" x-bind:checked="selectAll" >
+            </td>
+
+            <td class="px-6 py-3 text-sm">
+                <input class="rounded-lg" type="text" name="names[{{ $group }}]">
+            </td>
+
+            <td class="px-6 py-3 text-sm">
+                <input class="rounded-lg" type="text" name="descriptions[{{ $group }}]">
+            </td>
+
+            <td class="px-6 py-3 text-sm">
+                <code class="text-sm text-pink-500">
+                    {{ $group }}
+                </code>
+            </td>
+
+        </tr>
+    @endforeach
+
+@endsection
+
+@section('submit_button')
+    <x-button>{{ __('groups.import') }}</x-button>
+@endsection
+
+
+
+
+
+
