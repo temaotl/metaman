e00c76708238eca723c1a945dfab8ef5cb77a64d
Artem Otliaguzov <Artem.Otliaguzov@cesnet.cz>
2024-03-12 12:48:46 +0100

    fix textarea ( work only in laravel 8.x )

--- resources/views/entities/create.blade.php
+++ resources/views/entities/create.blade.php
@@ -4,14 +4,9 @@
 @section('back_button',route('entities.index'))
 @section('submit_button',__('entities.add'))
 @section('form_enctype',"multipart/form-data")
-
-
-@section('profile')
-    {{ __('entities.profile') }}
-@endsection
+@section('profile',__('entities.profile'))
 
 @section('specific_fields')
-
     <x-forms.create.create-form-field>
 
         <x-slot name="field">
@@ -30,7 +25,6 @@
 
     </x-forms.create.create-form-field>
 
-
     <x-forms.create.create-form-field>
 
         <x-slot name="field">
@@ -43,14 +37,21 @@
 
         <x-slot name="content">
 
-            <textarea class="text-sm @error('metadata') border @else @if (old('metadata') !== null) border-green-500 @endif @enderror focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm rounded-md"
+{{--            <textarea class="text-sm @error('metadata') border @else @if (old('metadata') !== null) border-green-500 @endif @enderror focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm rounded-md"
                       name="metadata" id="metadata" rows="10"
-                      placeholder="{{ __('entities.metadata_placeholder') }}">{{ old('metadata') }}</textarea>
+                      placeholder="{{ __('entities.metadata_placeholder') }}">{{ old('metadata') }}</textarea>--}}
+            <textarea @class([
+                         'text-sm focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm rounded-md',
+                         'border' => $errors->has('metadata'),
+                         'border-green-500' => old('metadata') !== null && !$errors->has('metadata'),
+                         ])
+                      name="metadata" id="metadata" rows="10"
+                      placeholder="{{ __('entities.metadata_placeholder') }}">{{ old('metadata') }}
+            </textarea>
 
         </x-slot>
 
     </x-forms.create.create-form-field>
-
 
     <x-forms.create.create-form-field>
 
@@ -64,7 +65,7 @@
 
         <x-slot name="content">
 
-            <select
+{{--            <select
                 class="text-sm @error('federation') border-red-500 border @else @if (old('federation') !== null) @endif @enderror focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm rounded-md"
                 name="federation" id="federation" required>
                 <option value="">{{ __('entities.choose_federation') }}</option>
@@ -73,7 +74,21 @@
                         {{ old('federation') == $federation->id ? 'selected' : '' }}>
                         {{ $federation->name }}</option>
                 @endforeach
+            </select>--}}
+
+            <select @class([
+                            'text-sm focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm rounded-md',
+                            'border-red-500 border' => $errors->has('federation'),
+                            ])
+                    name="federation" id="federation" required>
+                <option value="">{{ __('entities.choose_federation') }}</option>
+                @foreach ($federations as $federation)
+                    <option value="{{ $federation->id }}" {{ old('federation') == $federation->id ? 'selected' : '' }}>
+                        {{ $federation->name }}
+                    </option>
+                @endforeach
             </select>
+
 
         </x-slot>
 
@@ -91,17 +106,27 @@
         </x-slot>
 
         <x-slot name="content">
-            <textarea class="text-sm @error('explanation') border-red-500 border @else @if (old('explanation') !== null) @endif @enderror focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm rounded-md"
+
+{{--            <textarea class="text-sm @error('explanation') border-red-500 border @else @if (old('explanation') !== null) @endif @enderror focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm rounded-md"
                       name="explanation" id="explanation" rows="3" maxlength="255"
                       placeholder="{{ __('entities.explanation_placeholder') }}"
-                      required>{{ old('explanation') }}</textarea>
+                      required>{{ old('explanation') }}</textarea>--}}
+
+                        <textarea @class([
+                                     'text-sm focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm rounded-md',
+                                     'border-red-500 border' => $errors->has('explanation'),
+                                     'border-green-500' => !$errors->has('explanation') && old('explanation') !== null,
+                                     ])
+                                  name="explanation" id="explanation" rows="3" maxlength="255"
+                                  placeholder="{{ __('entities.explanation_placeholder') }}"
+                                  required>{{ old('explanation') }}
+                        </textarea>
+
 
 
         </x-slot>
 
     </x-forms.create.create-form-field>
-
-
 @endsection
 
 
