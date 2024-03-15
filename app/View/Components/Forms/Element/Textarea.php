09e8e669098056c828ab00afa7b21cfed440972d
Artem Otliaguzov <Artem.Otliaguzov@cesnet.cz>
2024-03-15 14:18:55 +0100

    make component for textarea and make entities edit.blade.php to new layout

---
+++ app/View/Components/Forms/Element/Textarea.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\View\Components\Forms\Element;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Textarea extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public $content;
+    public $err;
+    public function __construct($err,$content = null)
+    {
+        if(is_null($content))
+        {
+            $this->content = $err;
+        }
+        else
+        {
+            $this->content = $content;
+        }
+
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.forms.element.textarea');
+    }
+}
--- resources/views/entities/edit.blade.php
+++ resources/views/entities/edit.blade.php
@@ -1,53 +1,37 @@
-@extends('layout')
+@extends('layouts.defaultCreate')
 @section('title', __('entities.edit', ['name' => $entity->{"name_$locale"}]))
+@section('form_action', route('entities.update', $entity))
+@section('submit_button', __('entities.update'))
+@section('profile',__('entities.profile'))
+@section('form_enctype',"multipart/form-data")
 
-@section('content')
+@section('form_method')
+    @method('patch')
+    <input type="hidden" name="action" value="update">
+@endsection
 
-    <form action="{{ route('entities.update', $entity) }}" method="post" enctype="multipart/form-data">
-        @csrf
-        @method('patch')
-        <input type="hidden" name="action" value="update">
-        <div class="sm:rounded-lg mb-6 overflow-hidden bg-white shadow">
-            <div class="sm:px-6 px-4 py-5">
-                <h3 class="text-lg font-semibold">
-                    {{ __('entities.profile') }}
-                </h3>
-            </div>
-            <div class="border-t border-gray-200">
-                <dl>
-                    <div class="odd:bg-gray-50 even:bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 px-4 py-5">
-                        <dt class="text-sm text-gray-500">
-                            <label class="text-sm" for="file">
-                                {{ __('entities.metadata_file') }}
-                            </label>
-                        </dt>
-                        <dd class="sm:col-span-2">
-                            {!! $errors->first('file', '<div class="float-right text-sm font-semibold text-red-600">:message</div>') !!}
-                            <input
-                                class="focus:ring-blue-500 focus:border-blue-500 block w-full text-sm border-gray-300 rounded-md shadow-sm"
-                                type="file" name="file" id="file">
-                        </dd>
-                    </div>
-                    <div class="odd:bg-gray-50 even:bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 px-4 py-5">
-                        <dt class="text-sm text-gray-500">
-                            <label class="text-sm" for="metadata">
-                                {{ __('common.metadata') }}
-                            </label>
-                        </dt>
-                        <dd class="sm:col-span-2">
-                            {!! $errors->first('metadata', '<div class="float-right text-sm font-semibold text-red-600">:message</div>') !!}
-                            <textarea
-                                class="text-sm @error('metadata') border-red-500 border @else @if (old('metadata') !== null) border-green-500 @endif @enderror focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm border-gray-300 rounded-md"
-                                name="metadata" id="metadata" rows="10" placeholder="{{ __('entities.metadata_placeholder') }}">{{ old('metadata') }}</textarea>
-                        </dd>
-                    </div>
-                </dl>
-            </div>
-            <div class="px-4 py-4 bg-gray-100">
-                <x-buttons.back href="{{ URL::previous() }}" />
-                <x-button>{{ __('entities.update') }}</x-button>
-            </div>
-        </div>
-    </form>
+@section('specific_fields')
+    <x-forms.section.form-body-section
+        name="entities.metadata_file"
+        label="file"
+    >
+        <x-forms.element.input err="file">
+            type="file" name="file" id="file"
+        </x-forms.element.input>
+
+    </x-forms.section.form-body-section>
+
+    <x-forms.section.form-body-section
+        name="common.metadata"
+        label="metadata"
+    >
+        <x-forms.element.textarea err="metadata">
+            name="metadata" id="metadata" rows="10"
+            placeholder="{{ __('entities.metadata_placeholder') }}"
+        </x-forms.element.textarea>
+
+
+    </x-forms.section.form-body-section>
 
 @endsection
+
