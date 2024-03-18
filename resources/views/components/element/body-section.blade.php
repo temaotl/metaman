7b427824760dba31743d68401a3d738d4f4f62eb
Artem Otliaguzov <Artem.Otliaguzov@cesnet.cz>
2024-03-18 13:50:00 +0100

    update body-section component make it more universal

---
+++ resources/views/components/element/body-section.blade.php
@@ -0,0 +1,8 @@
+<div class="odd:bg-gray-50 even:bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 px-4 py-5">
+    <dt class="text-sm text-gray-500">
+        {{ $dt }}
+    </dt>
+    <dd class="sm:col-span-2">
+        {{ $dd ?? $slot }}
+    </dd>
+</div>
--- resources/views/components/forms/section/form-body-section.blade.php
+++ resources/views/components/forms/section/form-body-section.blade.php
@@ -1,13 +1,16 @@
-<div class="odd:bg-gray-50 even:bg-white sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6 px-4 py-5">
-    <dt class="text-sm text-gray-500">
+<x-element.body-section>
+    <x-slot name="dt">
         <label class="text-sm" for="{{ $label ?? 'name'  }}">
 
             {{ __($name) ?? "Here must be a name" }}
         </label>
-    </dt>
-    <dd class="sm:col-span-2">
-        {!! $errors->first( "$err" , '<div class="float-right text-sm font-semibold text-red-600">:message</div>') !!}
+    </x-slot>
 
-        {{ $slot  }}
-    </dd>
-</div>
+    <x-slot name="dd">
+        {!! $errors->first("$err", '<div class="float-right text-sm font-semibold text-red-600">:message</div>') !!}
+        {{ $slot }}
+    </x-slot>
+
+</x-element.body-section>
+
+
